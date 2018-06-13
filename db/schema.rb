96b4e46494aa0bddff3b14ae7478697cbab8783d
Younes Diouri <younesdiouri@gmail.com>
2018-06-13 18:25:12 +0200

    add username to devise && cascade delete to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_173836) do
+ActiveRecord::Schema.define(version: 2018_06_13_155353) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define(version: 2018_06_12_173836) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
