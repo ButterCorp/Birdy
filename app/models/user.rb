839034badb62d80c9efd3fb9587e61020743c0f1
Younes Diouri <younesdiouri@gmail.com>
2018-06-13 20:39:09 +0200

    followers update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :relationships, class_name:  "Relationship",
            foreign_key: "follower_id",
            dependent:   :destroy
+  has_many :following, through: :relationships, source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
