846fc217500bf7f553b3b5ba6dc1b4ce62045bef
Younes Diouri <younesdiouri@gmail.com>
2018-06-13 20:04:10 +0200

    user relationship method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
+  has_many :relationships, class_name:  "Relationship",
            foreign_key: "follower_id",
            dependent:   :destroy
   # Include default devise modules. Others available are:
