2ce1b19ab76c63821884833699705db699162bd1
Younes Diouri <younesdiouri@gmail.com>
2018-07-18 12:03:18 +0200

    image upload and resize working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,6 @@ class PostsController < ApplicationController
   end
 
     def post_params
-      params.require(:post).permit(:content, :user_id)
+      params.require(:post).permit(:content, :user_id, :picture)
     end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -27,7 +27,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
@@ -35,9 +34,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+  %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
